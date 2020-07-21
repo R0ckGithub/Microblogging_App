70873b458edbb9a5bc27eafac5feea19bb7ab30c
Ronak <upadhyayronak9313@gmail.com>
2020-07-21 23:58:19 +0530

    Using Recycler View From Android Studio

---
+++ app/src/main/java/com/example/mymedia/post_data.java
@@ -0,0 +1,129 @@
+package com.example.mymedia;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class  post_data {
+    String description;
+    String user_name;
+    Timestamp time_stamp;
+    String post_image_url;
+    String user_pic_url;
+    int likes_cnt;
+    int comment_cnt;
+    String post_id;
+    Boolean post_liked;
+
+
+    //utility functions
+
+    public void setUser_pic_url()
+    {
+        final String TAG = "User_pic_setter";
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db
+                .collection("users")
+                .document(user_name)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                user_pic_url = document.get("user_pic_url").toString();
+
+                                Log.d(TAG, "URLSnapshot data: " + user_pic_url);
+                            } else {
+                                Log.d(TAG, "No such URL document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public Timestamp getTime_stamp() {
+        return time_stamp;
+    }
+
+    public void setTime_stamp(Timestamp time_stamp) {
+        this.time_stamp = time_stamp;
+    }
+
+    public String getPost_image_url() {
+        return post_image_url;
+    }
+
+    public void setPost_image_url(String post_image_url) {
+        this.post_image_url = post_image_url;
+    }
+
+    public String getUser_pic_url() {
+        return user_pic_url;
+    }
+
+    public void setUser_pic_url(String user_pic_url) {
+        this.user_pic_url = user_pic_url;
+    }
+
+    public int getLikes_cnt() {
+        return likes_cnt;
+    }
+
+    public void setLikes_cnt(int likes_cnt) {
+        this.likes_cnt = likes_cnt;
+    }
+
+    public int getComment_cnt() {
+        return comment_cnt;
+    }
+
+    public void setComment_cnt(int comment_cnt) {
+        this.comment_cnt = comment_cnt;
+    }
+
+    public String getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(String post_id) {
+        this.post_id = post_id;
+    }
+
+    public Boolean getPost_liked() {
+        return post_liked;
+    }
+
+    public void setPost_liked(Boolean post_liked) {
+        this.post_liked = post_liked;
+    }
+}
